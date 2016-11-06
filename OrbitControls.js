ea5473fc614c36987fcefa0e768d15938f132ea7
guillaume <guillaumejparis@gmail.com>
2016-11-06 15:33:21 +0100

    Disable Pan and change mouse logic

--- OrbitControls.js
+++ OrbitControls.js
@@ -60,7 +60,7 @@ function OrbitControls ( object, domElement ) {
 	this.rotateSpeed = 1.0;
 
 	// Set to false to disable panning
-	this.enablePan = true;
+	this.enablePan = false;
 	this.keyPanSpeed = 7.0;	// pixels moved per arrow key push
 
 	// Set to true to automatically rotate around the target
@@ -75,7 +75,7 @@ function OrbitControls ( object, domElement ) {
 	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
 
 	// Mouse buttons
-	this.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.RIGHT };
+	this.mouseButtons = { ORBIT: THREE.MOUSE.RIGHT, ZOOM: THREE.MOUSE.MIDDLE };
 
 	// for reset
 	this.target0 = this.target.clone();
